96788437013607a2e4747aeef2dda514eb970a25
Abelxxxa <18001915100@163.com>
2024-12-22 20:50:54 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '48c2cb55-61eb-40fb-862d-b5ea50d71ff7';
 let proxyIP = '';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
